7c4e381232842967ca4bd815b95ce4542ad04edf
holiday <zx12as3420@gmail.com>
2021-09-16 10:11:54 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-while getopts ":i:p:n:o:h:s:" arg;do 
+while getopts ":i:p:n:o:s:" arg;do 
 	case "${arg}" in
 		i)preinput=${OPTARG};;
 		p)preplat=${OPTARG};;
 		n)prenor=${OPTARG};;
 		o)preout=${OPTARG};;
-		h)dinput=${OPTARG};;
 		s)dout=${OPTARG};;
 	esac
 done
 
 Rscript ./Script/Main.R -i $preinput -p $preplat  -n $prenor -o $preout
-~/opt/anaconda3/bin/python3 ./Script/HCCImm.py $dinput $dout
+~/opt/anaconda3/bin/python3 ./Script/HCCImm.py $preout $dout
 
 
 
